82cba470180de9016ce8f0bf295b0ee439061a53
Trung H. Tran <tranhoangtrung.nob@gmail.com>
2022-11-27 15:46:56 +0100

    :+1: init commit

--- src/nitrabot_control/src/bumper_control/bumper_control.py
+++ src/nitrabot_control/src/bumper_control/bumper_control.py
@@ -10,6 +10,8 @@
 
 # External library 
 import rospy
+from std_msgs.msg import Bool
+from geometry_msgs.msg import Twist
 
 # Internal library 
 
@@ -22,9 +24,50 @@ class BumperControl(object):
     # PUBLIC FUNCTION
     # ==========================================================================
     def __init__(self) -> None:
-        pass
+        """! Class constructor
+        """
+        # Initialization of ROS related variable
+        rospy.init_node("bumper_control_node", anonymous=True)
+
+        self.left_bumper_subscriber = rospy.Subscriber(
+            "/left_bumper", Bool, self._left_bumper_callback
+        )
+
+        self.left_bumper_subscriber = rospy.Subscriber(
+            "/right_bumper", Bool, self._right_bumper_callback
+        )
+
+        self.velocity_publisher = rospy.Publisher(
+            "/cmd_vel", Twist, queue_size=1)
+
+        self.control_timer = rospy.Timer(rospy.Duration(0.5), self._control_callback)
+
+        self._left_bumper_value = False
+
+        self._right_bumper_value = False
     
-    def run(self):
+    def run(self) -> None:
         """! Start ros node
         """
         rospy.spin()
+    
+    # ==========================================================================
+    # PRIVATE FUNCTION
+    # ==========================================================================
+    def _left_bumper_callback(self, msg: Bool) -> None:
+        """! Callback function for the left bumper rostopic.
+        @param[in] msg: odometry data in pose stamped form
+        """
+        self._left_bumper_value = msg.data
+
+    def _right_bumper_callback(self, msg: Bool) -> None:
+        """! Callback function for the right bumper rostopic.
+        @param[in] msg: odometry data in pose stamped form
+        """
+        self._right_bumper_value = msg.data
+
+    def _control_callback(self, event) -> None:
+        """! Callback function for the control timer.
+        @param[in] msg: odometry data in pose stamped form
+        """
+        pass
